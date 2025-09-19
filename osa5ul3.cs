328358ab01c017b87e45d2e46f7ec72b80a48c7f
IgorAleksejevTTHK <igoralekseje@gmail.com>
2025-09-19 09:54:30 +0300

    Create osa5ul3.cs

---
+++ osa5ul3.cs
@@ -0,0 +1,45 @@
+public class Ulesanne4
+    {
+        public static void FilmiAnalüüs()
+        {
+            List<Film> filmid = new List<Film>()
+            {
+                new Film("Matrix", 1999, "SciFi"),
+                new Film("The Astral", 1010, "Horror"),
+                new Film("Interstellar", 2014, "SciFi"),
+                new Film("Forrest Gump", 1994, "Drama"),
+                new Film("Smile 2", 2024, "Horror")
+            };
+            Console.WriteLine("Sisesta žanr ( SciFi, Horror, Drama ): ");
+            string zanr = Console.ReadLine();
+
+            var samaZanriga = filmid.Where(f => f.Zanr.Equals(zanr, StringComparison.OrdinalIgnoreCase)).ToList(); //все фильмы с один. жанром
+
+            if (samaZanriga.Count > 0)
+            {
+                Console.WriteLine($"Filmid žanrist {zanr}:");
+                foreach (var film in samaZanriga)
+                {
+                    Console.WriteLine($"{film.Nimi} ({film.Aasta})");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Žanris {zanr} filme ei leitud.");
+            }
+            var uusim = filmid.OrderByDescending(f => f.Aasta).First(); // по году
+            Console.WriteLine($"Kõige uuem film: {uusim.Nimi} ({uusim.Aasta}");
+            
+            Console.WriteLine("Filmid žanrite järgi:");
+            var grupp = filmid.GroupBy(f => f.Zanr); // groupby по фильм - жанр
+            foreach (var g in grupp) 
+            {
+                Console.WriteLine($"Žanr: {g.Key}"); // название жанра
+                foreach (var film in g)
+                {
+                    Console.WriteLine($"  {film.Nimi} ({film.Aasta})");
+                }
+            }
+        }
+    }
+}
